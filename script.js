c7d46a005b68c1f75cbc366ac9c8be6f3fdc836d
Socially Awkward Streams <candice.radam@gmail.com>
2022-10-19 14:50:07 -0500

    Added API link and a little JS.

---
+++ script.js
@@ -0,0 +1,6 @@
+//API KEY 959168e8af0d2f45700600a0d8d06e7f
+
+
+//fetching weather
+var search = document.getElementById("search_input")
+var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={959168e8af0d2f45700600a0d8d06e7f}'
