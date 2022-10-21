57e9db05a33fe50aec82ce3a2b23042ec51fb47a
Socially Awkward Streams <candice.radam@gmail.com>
2022-10-21 12:00:51 -0500

    Added forecast cards and fetch starter, and event listner.

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 var city=''
-var searchCity=
-var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?id=6167865&appid={959168e8af0d2f45700600a0d8d06e7f}'
-var api = 959168e8af0d2f45700600a0d8d06e7f
-var search = document.getElementById("search_input")
+var searchCity=document.getElementById("searchTxt").value;
+var apiKey = '959168e8af0d2f45700600a0d8d06e7f'
 
-fetch('https://api.openweathermap.org/data/2.5/weather?id=6167865&appid={959168e8af0d2f45700600a0d8d06e7f&units=imperial')
+
+
+var searchInput = document.getElementById("search_input")
+searchInput.addEventListener("click", getApi)
+
+function getApi() {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?id=6167865&appid={959168e8af0d2f45700600a0d8d06e7f}&units=imperial';
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+    
+
+      for (var i = 0; i < data.length; i++) {
+        console.log(data[i].name);
+      }
+    })}
+  
