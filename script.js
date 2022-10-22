651d2eee88d7a1339ad4e86e066f23752b096ed7
Socially Awkward Streams <candice.radam@gmail.com>
2022-10-22 11:21:51 -0500

    Finally got the search button to pull some data!

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 var city=''
-var apiKey = '959168e8af0d2f45700600a0d8d06e7f'
-var searchCity=document.getElementById("searchTxt").value;
-
+var apiKey = '959168e8af0d2f45700600a0d8d06e7f';
 
 var searchBtn = document.getElementById("#searchBtn")
-
-searchBtn.addEventListener("click", getApi)
+searchBtn.addEventListener('click', getApi);
 
 function getApi() {
     var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?id=6167865&appid=959168e8af0d2f45700600a0d8d06e7f';
-    fetch(requestUrl).then(function (response) {
+    fetch(requestUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function (data) {
-                console.log(data)})}
+                console.log(data)
+            })
+        }
+    })
+}
+
