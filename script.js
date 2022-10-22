8620703bfc4dfe8a1d84b7860d377e664bf2ae76
Socially Awkward Streams <candice.radam@gmail.com>
2022-10-22 16:06:24 -0500

    Adding some commenting and defined more variables to hopefully replace text.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-var city=''
+var otherCities = document.getElementById("#cities")
+var weatherDisplay = document.getElementById("#weather_display")
+var forecastCards = document.querySelectorAll("forecast_card")
+var searchBtn = document.getElementById("#searchBtn")
 var apiKey = '959168e8af0d2f45700600a0d8d06e7f';
 
-var searchBtn = document.getElementById("#searchBtn")
-searchBtn.addEventListener('click', getApi);
+//Search by city.
 
+searchBtn.addEventListener('click', getApi);
 function getApi() {
     var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?id=6167865&appid=959168e8af0d2f45700600a0d8d06e7f';
     fetch(requestUrl).then(function(response) {
@@ -15,5 +18,9 @@ function getApi() {
     })
 }
 
+//Displaying Current Forecast
+
 var currentForecast = document.getElementById('current_forecast')
 currentForecast.insertAdjacentText("afterend", "")
+
+//Forecast Cards
