bb66bbc71efbb7dba6a2a553a470746607d32c7e
Socially Awkward Streams <candice.radam@gmail.com>
2022-10-22 17:06:58 -0500

    Still trying to get search button to work.

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ var otherCities = document.getElementById("cities")
 var weatherDisplay = document.getElementById("weather_display")
 var forecastCards = document.querySelectorAll("#forecast_card")
 var searchBtn = document.querySelector("#searchBtn")
-var city = document.querySelector("#search_input").value
+var city = document.querySelector("#search_input")
+var cityValue = city.val()
 var apiKey = '959168e8af0d2f45700600a0d8d06e7f';
 
 //Search by city.
 
 
 
-searchBtn.addEventListener('click', getApi);
-function getApi() {console.log(city)
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/direct?q='+city.value+'&appid=959168e8af0d2f45700600a0d8d06e7f';
+searchBtn.on('click', getApi(city));
+function getApi(city) {
+    console.log(cityValue)
+    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='+city+'&appid='+apiKey;
     fetch(requestUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function (data) {
-                console.log(data)
-                console.log(data.list)
             })
         }
     })
