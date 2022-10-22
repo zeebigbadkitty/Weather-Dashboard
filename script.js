7195501fe86d66958a8b0b91e6c51862ab624fc8
Socially Awkward Streams <candice.radam@gmail.com>
2022-10-22 16:44:49 -0500

    trying to get search button to work again.

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-var otherCities = document.getElementById("#cities")
-var weatherDisplay = document.getElementById("#weather_display")
-var forecastCards = document.querySelectorAll("forecast_card")
-var searchBtn = document.getElementById("#searchBtn")
+var otherCities = document.getElementById("cities")
+var weatherDisplay = document.getElementById("weather_display")
+var forecastCards = document.querySelectorAll("#forecast_card")
+var searchBtn = document.querySelector("#searchBtn")
+var city = document.querySelector("#search_input").value
 var apiKey = '959168e8af0d2f45700600a0d8d06e7f';
 
 //Search by city.
 
+
+
 searchBtn.addEventListener('click', getApi);
-function getApi() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?id=6167865&appid=959168e8af0d2f45700600a0d8d06e7f';
+function getApi() {console.log(city)
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/direct?q='+city.value+'&appid=959168e8af0d2f45700600a0d8d06e7f';
     fetch(requestUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data)
+                console.log(data.list)
             })
         }
     })
@@ -21,6 +25,6 @@ function getApi() {
 //Displaying Current Forecast
 
 var currentForecast = document.getElementById('current_forecast')
-currentForecast.insertAdjacentText("afterend", "")
+
 
 //Forecast Cards
