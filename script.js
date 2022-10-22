647623bb31dbfc7127517fdca68a419daefd8159
Socially Awkward Streams <candice.radam@gmail.com>
2022-10-21 21:08:15 -0500

    Still trying to get the fetch and search button to work.

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 var city=''
-var searchCity=document.getElementById("searchTxt").value;
 var apiKey = '959168e8af0d2f45700600a0d8d06e7f'
+var searchCity=document.getElementById("searchTxt").value;
 
 
+var searchBtn = document.getElementById("#searchBtn")
 
-var searchInput = document.getElementById("search_input")
-searchInput.addEventListener("click", getApi)
+searchBtn.addEventListener("click", getApi)
 
 function getApi() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?id=6167865&appid={959168e8af0d2f45700600a0d8d06e7f}&units=imperial';
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-    
-
-      for (var i = 0; i < data.length; i++) {
-        console.log(data[i].name);
-      }
-    })}
-  
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?id=6167865&appid=959168e8af0d2f45700600a0d8d06e7f';
+    fetch(requestUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data)})}
